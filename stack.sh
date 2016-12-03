aca577ee34844ecb020613d249175c638be3aa27
Theo Pack Test <tf.pack@gmail.com>
2016-12-03 16:37:23 +0100

    Show stack description

--- stack.sh
+++ stack.sh
@@ -10,11 +10,13 @@ if ! aws cloudformation describe-stacks --stack-name ${STACKNAME} > /dev/null; t
     aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml \
         --parameters ParameterKey=TableName,ParameterValue=VoteTable \
         ParameterKey=StreamName,ParameterValue=VoteStream
+    aws cloudformation describe-stacks --stack-name ${STACKNAME}
 else
     aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml \
         --parameters ParameterKey=TableName,ParameterValue=VoteTable \
         ParameterKey=StreamName,ParameterValue=VoteStream \
         || echo "No Update"
+    aws cloudformation describe-stacks --stack-name ${STACKNAME}
 fi
 
 cfn-tail ${STACKNAME}
