1fb3f37291467a2c5456f8aa8ea3eb4032551c05
Theo Pack Test <tf.pack@gmail.com>
2016-12-03 10:10:15 +0100

    Add script to create stack

---
+++ stack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+export AWS_DEFAULT_REGION=eu-central-1
+
+STACKNAME=serverless-vote
+
+if ! aws cloudformation describe-stacks --stack-name ${STACKNAME} > /dev/null; then
+    aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml \
+        --parameters ParameterKey=TableName,ParameterValue=VoteTable \
+        ParameterKey=StreamName,ParameterValue=VoteStream
+else
+    aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name ${STACKNAME} --template-body file://cfn.yml \
+        --parameters ParameterKey=TableName,ParameterValue=VoteTable \
+        ParameterKey=StreamName,ParameterValue=VoteStream \
+        || echo "No Update"
+fi
+
+cfn-tail ${STACKNAME}
